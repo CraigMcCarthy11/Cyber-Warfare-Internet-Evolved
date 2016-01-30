a0c6bb3faa7760e41afaf6405a1424bcfacc2e96
CraigMcCarthy <craigmccarthy1994@gmail.com>
2016-01-30 14:58:51 -0500

    Menu Scripts and what not.

---
+++ Assets/Scripts/Menu Scripts/FactionSelect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactionSelect : MonoBehaviour {
+
+    public EnumManager.Faction chosenFaction;
+
+    public void ButtonPressed()
+    {
+        PlayerPrefs.SetString("Player Faction", chosenFaction.ToString());
+
+        Debug.Log(PlayerPrefs.GetString("Player Faction"));
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartedGame()
+    {
+        //Gets the faction string
+        string playerFaction = PlayerPrefs.GetString("Player Faction");
+        //If its set, we can start the game
+        if (playerFaction != null){
+            Application.LoadLevel(1);
+        }
+    }
+}
