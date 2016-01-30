28291b18f7ce0353f24300d72abe3941bbc70fb9
Matthew Bolles <matthewbolles@att.net>
2016-01-29 22:22:31 -0500

    Placing fixes with new camera.

--- Assets/Scripts/BuildingPlacer.cs
+++ Assets/Scripts/BuildingPlacer.cs
@@ -5,72 +5,99 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.EventSystems;
 
+
 [Serializable]
+// Stores both the button and the object for a specific building/structure
 public struct PlaceableStructure
 {
     public GameObject buttonPrefab;
     public GameObject structurePrefab;
 }
 
+/// <summary>
+/// Apparently places buildings
+/// Spawns buttons based on the list in the inspector
+/// Spawns structures when buttons are pressed
+/// </summary>
 public class BuildingPlacer : MonoBehaviour {
 
     public EventSystem evtSystem;
     public RectTransform selectionRect;
     public List<PlaceableStructure> placeableStructures = new List<PlaceableStructure>();
-    public GameObject currentItem;
+    private GameObject currentItem;
     private LayerMask mask; 
 
 	// Use this for variable initialization
-	void Start () {
-        for(int i = 0; i < placeableStructures.Count; i++)
+	void Start ()
+    {
+        // Get the correct layer for colliding with
+        mask = LayerMask.NameToLayer("Terrain");
+
+        // Spawn all the building buttons
+        for (int i = 0; i < placeableStructures.Count; i++)
         {
             int indx = i;
             GameObject btnObj = Instantiate(placeableStructures[i].buttonPrefab) as GameObject;
             btnObj.transform.SetParent(selectionRect);
 
+            // Set the delegate
             btnObj.GetComponent<Button>().onClick.AddListener(() => { BeginPlacing(indx); });
         }
-        
-        mask = LayerMask.NameToLayer("Terrain");
     }
 
+    /// <summary>
+    /// Spawns our structure in response to a button click
+    /// </summary>
+    /// <param name="index"> this is the idex of the structure we will be placing </param>
     public void BeginPlacing(int index)
     {
         currentItem = Instantiate(placeableStructures[index].structurePrefab) as GameObject;
-        currentItem.transform.localScale = Vector3.one;
         currentItem.transform.position = new Vector3(0, 100, 0);
         currentItem.transform.rotation = Quaternion.identity;
-        Debug.Log("Placing begin");
+
+        // Begin dragging around the item
         StartCoroutine(UpdatePlacing());
     }
 
+    /// <summary>
+    /// Update method for placing a building
+    /// </summary>
     IEnumerator UpdatePlacing()
     {
-        while(true)
+        // Always true loop, break to escape
+        while (true)
         {
-            Debug.Log("Placing");
             // Dont bother continuing if current item is null;
-            if(currentItem == null)
+            if (currentItem == null)
+            {
                 break;
+            }
             
+            // If we hit our ray on the layer, update the position of our building.
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, mask))
+
+            int layerMask = 1 << mask.value;
+
+            // Does the ray intersect any objects which are in the player layer.
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
                 currentItem.transform.position = hit.point;
             }
 
+            // If we click down...
             if(Input.GetMouseButtonDown(0))
             {
+                // When over any form of UI
                 if(evtSystem.IsPointerOverGameObject())
                 {
-                    Debug.Log("Failed");
+                    // Disable placing
                     EndPlacing(false);
                     break;
                 }
                 else
                 {
-                    Debug.Log("Success");
+                    // Place our building
                     EndPlacing(true);
                     break;
                 }
@@ -79,22 +106,24 @@ public class BuildingPlacer : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Unlinks our current object and either places or destroys it
+    /// </summary>
+    /// <param name="placeAtLocation"> determines if we cancel or finish placement </param>
     void EndPlacing(bool placeAtLocation)
     {
-        Debug.Log("End");
+        // If we are not placing...
         if (!placeAtLocation)
         {
+            // Blow that shit UP!!!
             Destroy(currentItem);
         }
         else
         {
+            // Enable the collider for normal function
             currentItem.GetComponent<BoxCollider>().enabled = true;
-        }
-        currentItem = null;
+        } 
+        
+        currentItem = null; // Stop holding the item
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
