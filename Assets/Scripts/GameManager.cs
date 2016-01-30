7756c98bcc25e4b6f7e8f0510a0e05fc3c1c07a3
CraigMcCarthy <craigmccarthy1994@gmail.com>
2016-01-29 19:24:38 -0500

    Game and Audio manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private static GameManager thisInstance;
+
+    #region Singleton Stuff
+    /// <summary>
+    /// Constructor that handles getting and setting the instance
+    /// this is using the singleton pattern
+    /// </summary>
+    public static GameManager instance
+    {
+        get
+        {
+            if (thisInstance == null)
+            {
+                thisInstance = GameObject.FindObjectOfType<GameManager>();
+
+                //Tell unity not to destroy this object when loading a new scene!
+                DontDestroyOnLoad(thisInstance.gameObject);
+            }
+            return thisInstance;
+        }
+    }
+
+    void Awake()
+    {
+        if (thisInstance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            thisInstance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            //If a Singleton already exists and you find
+            //another reference in scene, destroy it!
+            if (this != thisInstance)
+                Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
 }
